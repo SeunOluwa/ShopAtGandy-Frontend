a75f5b732ed5c988493786f09eda916859742b0c
Oluwaseun Taiwo <taiwoluwaseun8@gmail.com>
2022-09-29 13:31:41 +0100

    dashboard css cleanup

--- src/components/Admin/Dashboard.js
+++ src/components/Admin/Dashboard.js
@@ -29,42 +29,36 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="flex justify-between items-center py-8 px-40">
-        <p className="text-3xl font-extrabold">Shop@Gandy Dashboard</p>
+      <div className="dashboard-header">
+        <p className="dashboard-logo">Shop@Gandy Dashboard</p>
 
-        <button className="text-xl p-3 border-2 rounded-md border-red-500 bg-red-500 text-white">
+        <button className="upload-products-btn">
           <Link to="/admin/upload-product">UPLOAD PRODUCTS</Link>
         </button>
       </div>
 
-      <div className="py-8 px-40">
+      <div className="products">
         {loading ? (
           <h3>Loading...</h3>
         ) : error ? (
           <h3>{error}</h3>
         ) : (
-          <div className="mt-8 grid grid-cols-3 gap-10">
+          <div className="product">
             {products.map((product) => (
-              <div className="card hover:shadow-lg" key={product._id}>
+              <div className="card" key={product._id}>
                 <img
                   src={product.image}
                   alt={product.name}
-                  className="w-fit h-fit object-cover"
+                  className="product-img"
                 />
-                <div className="m-4 border-b border-gray-300 pb-2">
-                  <span className="font-bold text-lg">{product.name}</span>
-                  <span className="block text-gray-500 text-base">
-                    {product.details}
-                  </span>
-                  <span className="font-bold text-xl">₦{product.price}</span>
+                <div className="product-info">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-details">{product.details}</span>
+                  <span className="product-price">₦{product.price}</span>
                 </div>
-                <div className="m-4 flex justify-between items-center">
-                  <button className="font-bold text-center text-base p-2 border rounded border-blue-500 text-white bg-blue-500 hover:bg-blue-700 transition ease-in-out duration-500">
-                    EDIT PRODUCT
-                  </button>
-                  <button className="font-bold text-center text-base p-2 border rounded border-red-500 text-white bg-red-500 hover:bg-red-700 transition ease-in-out duration-500">
-                    REMOVE PRODUCT
-                  </button>
+                <div className="product-edit-remove-btns">
+                  <button className="product-edit-btn">EDIT PRODUCT</button>
+                  <button className="product-remove-btn">REMOVE PRODUCT</button>
                 </div>
               </div>
             ))}
